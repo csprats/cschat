1cd6837de1c20b3f8b4e8404add023dfe26d9248
csprats <222044220+csprats@users.noreply.github.com>
2025-08-19 16:57:30 +0200

    Add privacity

--- src/Sesion.jsx
+++ src/Sesion.jsx
@@ -2,60 +2,54 @@ import React, { useState, useContext } from 'react';
 import { UserContext } from './UserContext.jsx';
 
 export const Sesion = () => {
-  const { saveUserName } = useContext(UserContext);
-  const [open, setOpen] = useState(false);
-  const [inputValue, setInputValue] = useState('');
+  const { saveUserName } = useContext(UserContext)
+  const [open, setOpen] = useState(true)
+  const [inputValue, setInputValue] = useState('')
+  const [inputPasswordValue, setPasswordValue] = useState('')
 
   const handleOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-    saveUserName(inputValue);
-    setInputValue('');
-  };
+    if (inputPasswordValue != 'cl') return
+    setOpen(false)
+    saveUserName(inputValue)
+    setInputValue('')
+  }
 
   const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
+    setInputValue(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPasswordValue(e.target.value)
+  }
 
   return (
     <div>
       <button onClick={handleOpen}>Enter a username</button>
       {open && (
-        <div style={{
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'rgba(0,0,0,0.5)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          <div style={{
-            backgroundColor: 'black',
-            padding: '20px',
-            borderRadius: '5px',
-            boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
-            height: '100px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }}>
-            <p style={{ color: 'white' }}>Write your name:</p>
+        <div className='background'>
+          <div className='card'>
+            <p style={{ color: 'white' }}>Enter the correct chat password:</p>
             <input
+              className='name'
+              type="text"
+              value={inputPasswordValue}
+              onChange={handlePasswordChange}
+            />
+            <p style={{ color: 'white' }}>And write your name:</p>
+            <input
+              className='name'
               type="text"
               value={inputValue}
               onChange={handleChange}
-              style={{ width: '200px', margin: '10px 0' }}
             />
             <button onClick={handleClose}>Save</button>
           </div>
         </div>
       )}
     </div>
-  );
+  )
 }
